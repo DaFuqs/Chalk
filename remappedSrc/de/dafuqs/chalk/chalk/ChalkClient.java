fdf2fbd6798794001657c3dea8b355ceb2cf6590
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2022-12-10 03:24:12 +1300

    Updated remappedSrc to 1.19.3 port

--- remappedSrc/de/dafuqs/chalk/chalk/ChalkClient.java
+++ remappedSrc/de/dafuqs/chalk/chalk/ChalkClient.java
@@ -3,8 +3,6 @@ package de.dafuqs.chalk.chalk;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
 public class ChalkClient implements ClientModInitializer {
